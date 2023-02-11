1cfe052b38c95a7eedeaa67cd162d2238ca54bed
Diana <kat.lahuro.3223@gmail.com>
2023-02-11 17:50:56 +0300

    2

---
+++ second/2.cs
@@ -0,0 +1,20 @@
+﻿float x, y, b1, b2, k1, k2;
+System.Console.WriteLine("Введите b1");
+b1=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите b2");
+b2=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите k1");
+k1=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите k2");
+k2=Convert.ToInt32(Console.ReadLine());
+
+if (k1==k2)
+{
+System.Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+x = ((b2 - b1) / (k1 - k2));
+y = k1*x+b1;
+System.Console.WriteLine($"({x};{y})");
+}
